49d7d8ac467cff88205a81f7059e48ff63c35c24
demenyzsofia <79308025+demenyzsofia@users.noreply.github.com>
2022-04-21 18:15:56 +0300

    correction of home page

--- webapp/src/pages/home/home.js
+++ webapp/src/pages/home/home.js
@@ -6,14 +6,16 @@ const Home = () => {
     const { t } = useTranslation(); 
     return <div className="content">
                 <h2 id="scope">{t("pages.home.title")}</h2>
-                <p>{t("pages.home.introduction")}</p>
-                <p>{t("pages.home.content1")}<a href="/datasets"><b><i>{t("pages.home.datasets")}</i></b></a>{t("pages.home.content2")}</p>
-                <ol className="list">
-                    <li><b><i>{t("pages.home.listing.1")}</i></b></li>
-                    <li><b><i>{t("pages.home.listing.2")}</i></b></li>
-                    </ol>                    
-                <p>{t("pages.home.data")}</p>
-                <p>{t("pages.home.performance")}</p>                   
+                <div className="text">
+                    <p>{t("pages.home.introduction")}</p>
+                    <p>{t("pages.home.content1")}<a href="/datasets"><b><i>{t("pages.home.datasets")}</i></b></a>{t("pages.home.content2")}</p>
+                    <ol className="list">
+                        <li><b><i>{t("pages.home.listing.dataAnalysis")}</i></b></li>
+                        <li><b><i>{t("pages.home.listing.performanceAnalysis")}</i></b></li>
+                        </ol>                    
+                    <p>{t("pages.home.data")}</p>
+                    <p>{t("pages.home.performance")}</p>      
+                </div>             
         </div> 
         
 }
